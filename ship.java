1cecb1695f8be40564500ecf0aecf909239e0484
DOTxStorM <niklee1212@gmail.com>
2013-09-20 10:41:06 -0700

    added a whole bunch of shit

--- ship.java
+++ ship.java
@@ -1,10 +1,40 @@
 class ship{
+	float m_shields;
+	float m_armor;
+	float m_currRotation;
+	float m_movementSpeed;
+	float m_rotationSpeed;
 	
-	public void ship(){
-		float m_shields;
-		float m_health;
-		float m_rotation;
-		float m_speed;
+	int posX;
+	int posY;
+	
+	boolean bIsRotation = false;
+	boolean bIsMoving = false;
+	public ship(float shields,
+		float armor,
+		float spawnRotation,
+		float movementSpeed,
+		float rotationSpeed,
+		int spawnX,
+		int spawnY){
+		
+		m_shields = shields;
+		m_armor = armor;
+		m_currRotation = spawnRotation;
+		m_movementSpeed = movementSpeed;
+		m_rotationSpeed = rotationSpeed;
+		
+		posX = spawnX;
+		posY = spawnY;
+	}
+	
+	public int[] getLocation(){
+		int[] toReturn = new int[2];
+		
+		toReturn[0] = posX;
+		toReturn[1] = posY;
+		
+		return toReturn;
 	}
 	
 	public static void main(String argv[]){
